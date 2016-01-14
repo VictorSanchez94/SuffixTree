8605d81b4e4db19fd226cfd8bc4fccb20fbd3b01
VictorSanchez94 <vs_zgz94@hotmail.com>
2016-01-14 10:50:21 +0100

    Interfaz

--- src/suffixTree/SuffixTree.java
+++ src/suffixTree/SuffixTree.java
@@ -11,14 +11,29 @@ public class SuffixTree {
 		if(args.length == 2){
 			String pattern = args[0];
 			String text = args[1];
-			SimpleSuffixTree sTree = new SimpleSuffixTree("text");
+			System.out.println("Creando arbol compacto de sufijos...");
+			SimpleSuffixTree sTree = new SimpleSuffixTree(text);
 			CompactSuffixTree cTree = new CompactSuffixTree(sTree);
+			System.out.println("Buscando patron...");
+			ArrayList<Integer> list = cTree.searchAll(pattern);
+			if(list.isEmpty()){
+				System.out.printf("No se ha encontrado el patrón '%s' en el texto.\n", pattern);
+			}else if(list.size() == 1){
+				System.out.printf("El patron '%s' aparece en la posicion %d del texto.\n", pattern, list.get(0));
+			}else{
+				System.out.printf("El patron '%s' aparece en el texto en las posiciones:\n", pattern);
+				System.out.print("\t");
+				for (Integer i : list){
+					System.out.printf("%d ", i);
+				}
+			}
 		}else if(args.length >= 3){
 			String pattern = args[0];
 			int numTexts = Integer.parseInt(args[1]);
 			if(args.length == (numTexts+2)){
-				for(int i=3; i<args.length; i++){
-					
+				String[] texts = new String[numTexts];
+				for(int i=3, j=0; i<args.length; i++, j++){
+					texts[j] = args[i];
 				}
 			}else{
 				System.err.println("ERROR. Numero de textos introducido como segundo parametro no coincide con el numero\n"
